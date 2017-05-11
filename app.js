7583787e6a4e48277b802d52c880efb5d818e72f
bmheller <bmariedevelopment@gmail.com>
2017-05-11 00:53:36 +0000

    added mongolab database

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://britt2:password@ds137291.mlab.com:37291/webdevyelpcampapp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
